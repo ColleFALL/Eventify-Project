4e11256b6ee79b7bbeb46fdac3eb6d9afac34ade
ColleFALL <collefall118@gmail.com>
2025-10-15 23:53:24 +0000

    integration front-end parti connexion 15/10/2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
-import Register from "./Pages/Auth/Register";
-// import Login from "./Pages/Auth/Login";
+// import Register from "./Pages/Auth/Register";
+import Login from "./Pages/Auth/Login";
 
 
 function App() {
   return (
-    <Register/>
-    // <Login/>
+    // <Register/>
+    <Login/>
   );
 }
 
