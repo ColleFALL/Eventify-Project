705ebcca81d0703921fccc78aaf85ffadac8ab6c
barbienoir <diawendieme@gmail.com>
2025-10-23 17:21:20 +0000

    voici le navbar 23/10

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+
+
 
 function App() {
   return (
-    <>
-    <h1 class="text-3xl bg-gray-600 font-bold underline">Bonjour l'equipe</h1>
-
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-</svg>
-
-    </>
-  )
+    
+    <Navbar/>
+    // <Footer/>
+      //  <Routes>
+          
+            
+      //       <Route path="/Navbar" element={<Navbar />} />
+      //       <Route path="/Footer" element={<Footer />} />
+      //     </Routes>
+    
+  );
 }
 
-export default App
+export default App;
